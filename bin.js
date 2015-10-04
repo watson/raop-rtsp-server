d04e3f82427e5c20ed2ac2bed277c81fbce0c1d3
Thomas Watson Steen <w@tson.dk>
2015-10-05 00:45:22 +0200

    Fix CLI

--- bin.js
+++ bin.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict'
 
-var server = require('../')
+var server = require('./')
 var stdout = process.argv[2] === '--stdout'
 
 server.sessions.on('new', function (session) {
