3f6520458e74fe0897d05854f20033aeb822aa72
Thomas Watson Steen <w@tson.dk>
2015-10-06 01:35:09 +0200

    Add native speaker support

--- bin.js
+++ bin.js
@@ -2,16 +2,31 @@
 'use strict'
 
 var server = require('./')
-var stdout = process.argv[2] === '--stdout'
+var dest
+
+if (process.argv[2] === '--stdout') {
+  dest = process.stdout
+} else {
+  try {
+    var Speaker = require('speaker')
+    dest = new Speaker({
+      channels: 2,
+      bitDepth: 16,
+      sampleRate: 44100
+    })
+  } catch (e) {
+    console.error('Warning: Could not load speaker module')
+  }
+}
 
 server.sessions.on('new', function (session) {
-  if (!stdout) return session.resume()
+  if (!dest) return session.resume()
 
   session.on('data', function (chunk) {
     for (var offset = 0, blocks = chunk.length; offset < blocks; offset += 2) {
       chunk.writeInt16LE(Math.round(chunk.readInt16LE(offset) * session.volume), offset)
     }
-    process.stdout.write(chunk)
+    dest.write(chunk)
   })
 })
 
