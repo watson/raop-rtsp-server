6fdd913a6ae90bd2819efb5d30acf6523422c28c
Thomas Watson Steen <w@tson.dk>
2015-10-06 01:02:25 +0200

    Use a fake MAC address
    OSX doesn't like if the RAOP server uses the real MAC address for some
    reason. Maybe it's because it detects that it belongs to the same
    computer as the it self and finds that to be evidence of spoofing.

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 var rtsp = require('rtsp-server')
 var mdns = require('raop-mdns-server')
 var xtend = require('xtend')
+var randomMac = require('random-mac')
 var rtspMethods = require('./lib/rtsp-methods')
 var debug = require('./lib/debug')
 var pkg = require('./package')
 
 var serverAgent = 'AirTunes/105.1'
+
+global._raopMacAddr = randomMac()
 
 exports.sessions = require('./lib/sessions')
 
@@ -49,9 +52,8 @@ exports.start = function (opts) {
       sf: '0x0'
     }
 
-    mdns({ name: opts.name, port: port, txt: txt }, function (err, result) {
+    mdns({ name: opts.name, port: port, txt: txt, mac: global._raopMacAddr }, function (err, result) {
       if (err) throw err
-      global._raopMacAddr = result.mac
     })
   })
 
